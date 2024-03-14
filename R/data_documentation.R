7fbc1b472b8a67be4086dfa331e5d6bbec36bec6
GrigoriiNos <nosgrigoriy@gmail.com>
2024-03-14 15:14:41 +0100

    added plotting funcs and vignette

---
+++ R/data_documentation.R
@@ -0,0 +1,13 @@
+#' Viral screen, data example for pmhcDenoiseR
+#'
+#' A dataset containing information about something interesting.
+#'
+#' @format A data frame with 50 rows and 2 variables:
+#' \describe{
+#'   \item{viral_screen}{PBMC T cells, screen for chronic viral epitopes}
+#' }
+#' @source Source of your data (Sine Reker Hadrup lab).
+#' @examples
+#' data(viral_screen)
+#' summary(viral_screen)
+"viral_screen"
